59d2942608189534cac0c17382197443ddb5e7cb
Phronesislex <alexanderagyenimboateng@gmail.com>
2018-10-22 20:02:12 +0000

    added configure.php

---
+++ membertest.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 22, 2018 at 01:54 PM
+-- Server version: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `membertest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE IF NOT EXISTS `members` (
+  `ID` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`ID`, `username`, `password`, `email`) VALUES
+(1, 'test1', 'test1', 'test1@test1.com'),
+(2, 'testTwo', 'testTwo', 'test2@test2.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ testPrep2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A BASIC HTML FORM</title>
+
+<?PHP 
+$email = "";
+$passW = "";
+$uName = "";
+$date1 = "";
+if (isset($_POST['Submit1'])){
+	require 'configure.php';
+
+	$uName = $_POST['user'];
+	$email = $_POST['email'];
+	$passW = $_POST['pass'];
+	$date1 = $_POST['date1'];
+
+	$database = $_POST['membertest'];
+	$db_found = new mysqli (DB_SERVER, DB_USER, DB_PASS, $database);
+
+	if ($db_found) {
+		$SQL = $db_found->prepare("INSERT INTO members (username, password,email, date1) VALUES(?, ?, ?, ?)");
+		$SQL->bind_param('ssss', $uName, $passW, $email, $date1);
+		$SQL->execute();
+		$SQL->close();
+		$db_found->close();
+		print "New row inserted";
+
+	}
+
+	else
+		{
+			print "Database NOT found";
+		}
+	}
+?>
+
+</head>
+<body>
+	<FORM NAME ="form1" METHOD ="POST" ACTION ="testPrep2.php">
+	user <input type="TEXT" name="user" value="test2"><BR>
+	Pass <input type="TEXT" name="pass" value="test2"><BR>
+	email address <input type="TEXT" name="email" value="<?php print $email; ?>">	
+	Date <input type="date" name="date1" value="Enter Date"> <BR>
+
+	<input type = "Submit" Name = "Submit1" value = "Login"> 
+	</FORM>
+
+</body>
+</html>
